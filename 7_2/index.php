2bd9590c004466e911994eecc35c777103288576
mc-hasegawa <hasegawa010515@gmail.com>
2016-07-25 11:42:16 +0900

    入力確認処理修正

--- 7_2/index.php
+++ 7_2/index.php
@@ -11,10 +11,7 @@ if (isset($_POST["posting_content"]))
 	{
 		$user_name = "匿名希望";
 	}
-	// var_dump($user_name);
-	// var_dump($content);
 	fseek($file,0,SEEK_END);
-	print_r(ftell($file));
 	fwrite($file,"\n***>>");
 	fwrite($file,htmlspecialchars($user_name."\n"));
 	fwrite($file,"date>>");
@@ -39,10 +36,14 @@ $file_line_count = 1;
 <script>
 function posting_check()
 {
+	console.log(document.posting_form.posting_button.disabled);
 	if(document.posting_form.posting_content.value == "")
 	{
-		window.alert('投稿内容がありません');
-		return false;
+		document.posting_form.posting_button.disabled = "true";
+	}
+	else
+	{
+		document.posting_form.posting_button.disabled = "";
 	}
 }
 </script>
@@ -96,12 +97,12 @@ function posting_check()
 		}
 		?>
 		======================================<br>
-		<form action="" method="post" name="posting_form" onSubmit="return posting_check()">
+		<form action="" method="post" name="posting_form">
 			名前
 			<p><input type="text" name="posting_user_name" value=""></p>
 			書き込む内容
-			<p><textarea name="posting_content" rows="6" cols="40" wrap="hard"></textarea></p>
-			<input type="submit" value="書き込む">
+			<p><textarea name="posting_content" rows="6" cols="40" wrap="hard" onChange="posting_check()"></textarea></p>
+			<input name="posting_button" type="submit" value="書き込む" disabled>
 		</form>
 	</p>
 </body>
